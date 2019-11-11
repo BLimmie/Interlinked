12f401111fd8fed029f845694efa22819f4c5b7f
BLimmie <brian01.lim@gmail.com>
2019-11-11 15:29:47 -0800

    added worker parallelization

--- app/model.go
+++ app/model.go
@@ -1,7 +1,20 @@
 package app
 
-import "go.mongodb.org/mongo-driver/bson/primitive"
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"sync"
+)
 
 type LoginHandler struct {
 	CurrentTokens map[string]primitive.ObjectID
+}
+
+type ResultStruct struct {
+	result interface{}
+	err    error
+}
+
+type WorkerHandler struct {
+	Workers chan bool
+	mux     sync.Mutex
 }
---
+++ app/workers.go
@@ -0,0 +1,29 @@
+package app
+
+import (
+	"errors"
+	"sync"
+)
+
+func NewWorkerHandler(numWorkers int) *WorkerHandler {
+	return &WorkerHandler{
+		Workers: make(chan bool, numWorkers),
+		mux:     sync.Mutex{},
+	}
+}
+
+func (wh *WorkerHandler) SubmitJob(resultChan chan ResultStruct, f func() (interface{}, error)) error {
+	wh.mux.Lock()
+	select {
+		case wh.Workers <- true:
+		default:
+			return errors.New("all workers busy")
+	}
+	wh.mux.Unlock()
+	go func() {
+		res, err := f()
+		resultChan <- ResultStruct{res, err}
+		<-wh.Workers
+	}()
+	return nil
+}
