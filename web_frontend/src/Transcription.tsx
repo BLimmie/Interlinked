4ed5da402dc312e7feb84aaadb692d1bc003d294
Gehrig Weber <gehrigweber@umail.ucsb.edu>
2019-11-24 17:52:06 -0800

    GW: Only calls server once per phrase

--- web_frontend/src/Transcription.tsx
+++ web_frontend/src/Transcription.tsx
@@ -3,17 +3,26 @@ import SpeechRecognition from "react-speech-recognition";
 import Speech from './Speech';
 
 let sentiment_num: number = 0;
+let tones: boolean[] = [];
+let tones2: boolean[] = [];
+let calledyet: boolean[] = [];
 
-function interpret_sentiment(sentiment: string) {
+function sleep(ms: any) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function interpret_sentiment(sentiment: string, i: number) {
   var temp_s = sentiment.substring(13, sentiment.length - 2)
   sentiment_num = parseFloat(temp_s);
+  await sleep(1000);
+  tones[i] = true;
   return null;
 }
 
-function httpCall(method: string, url:string, data:any, callback:(result:any)=>any) {
+function httpCall(method: string, url:string, data:any, callback:(result:any, r:any)=>any, i: number) {
   var xhr = new XMLHttpRequest();
   xhr.open(method, url, true);
-  if (callback) xhr.onload = function() { callback(this['responseText']); };
+  if (callback) xhr.onload = function() { callback(this['responseText'], i); };
   if (data != null) {
       xhr.setRequestHeader('Content-Type', 'text/plain');
       xhr.send(data);
@@ -42,43 +51,45 @@ class Transcription extends React.Component<SpeechProps> {
         return null
       }
 
-    /*  
-    // Extracting the most recent phrase and determining whether to add it or not,
-    // only adding it if there's no overlap with the previous phrase.
-    const transcript_split = the_props.transcript.split(' ');
-    const recent_phrase = transcript_split.slice(-12)
-
-    if (transcript_split.length > 12 * (globalThis.phrase_count + 1)) {
-        globalThis.words.set(recent_phrase.join(" "), getRandomInt(0,2));
-    }
-    */
-
-    // Dynamically updating transcript, but in a rather inefficient way
+    // Dynamically updating transcript
     const transcript_split = the_props.transcript.split(' ');
     let temp = Array.from( globalThis.words.values() );
+
     globalThis.words = new Map();
 
     let limit = Math.floor(transcript_split.length / 12);
     if (transcript_split.length % 12 !== 0) {
       limit++;
     }
+    if (tones.length < limit) {
+      tones.length = limit;
+    }
+    if (tones2.length !== tones.length) {
+      tones2.length = tones.length;
+    }
+    if (calledyet.length !== tones.length) {
+      calledyet.length = tones.length;
+    }
     for (var i = 0; i < limit; i++) {
       if (i !== limit - 1 && i !== limit - 2) {
         globalThis.words.set(transcript_split.slice(12 * i, 12 * (i + 1)).join(" "), temp[i]);
       }
-      else if (i === limit - 2 && transcript_split.length % 12 < 3) {
+      else if (i === limit - 2 && calledyet[i] !== true) {
         let recent_phrase: string = transcript_split.slice(12 * i, 12 * (i + 1)).join(" ");
-        httpCall('POST', "http://localhost:8080/sentiment/text", recent_phrase, interpret_sentiment);
-
+        httpCall('POST', "http://localhost:8080/sentiment/text", recent_phrase, interpret_sentiment, i);
+        calledyet[i] = true;
+      }
+      else if (i === limit - 2 && tones[i] === true && tones2[i] !== true) {
         let tone: number = 0; // Neutral
-        if (sentiment_num > 0) {
+        if (sentiment_num > 0.1) {
           tone = 1; // Positive
         }
-        else if (sentiment_num < 0) {
+        else if (sentiment_num < -0.1) {
           tone = 2; // Negative
         }
-
+        let recent_phrase: string = transcript_split.slice(12 * i, 12 * (i + 1)).join(" ");
         globalThis.words.set(recent_phrase, tone);
+        tones2[i] = true;
       }
       else if (i === limit - 1) {
         globalThis.words.set(transcript_split.slice(12 * i).join(" "), 0);
