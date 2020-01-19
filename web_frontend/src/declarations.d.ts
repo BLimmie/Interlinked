8f5bd40fc30cf2231a9af8c80ad6d7aad70a309b
Michael Zhou <yihang_zhou@ucsb.edu>
2020-01-19 15:32:07 -0800

    removed web_frontend

--- web_frontend/src/declarations.d.ts
+++
@@ -1,5 +0,0 @@
-// For all those libraries without a @types/ folder
-
-declare module 'react-speech-recognition';
-
-declare module 'react-alert-template-oldschool-dark';
--- web_frontend/src/globals.d.ts
+++
@@ -1,9 +0,0 @@
-var words;
-var display_words;
-var sentiment;
-var point_in_transcript;
-var phrase_count;
-
-// This should be the raw output string from the server/sentiment/frame, something like
-// '{"AU":{"Blink":0,"Brow Lowerer":0,"Cheek Raiser":3.62,"Chin Raiser":0,"Dimpler":1.46,"Inner Brow Raiser":0,"Jaw Drop":1.9,"Lid Tightener":2.91,"Lip Corner Depressor":0.24,"Lip Corner Puller":3.44,"Lip Stretcher":0,"Lip Tightener":0,"Lips Part":2.65,"Nose Wrinkler":1.46,"Outer Brow Raiser":0,"Upper Lid Raiser":0,"Upper Lip Raiser":2.87},"Emotion":{"anger":"VERY_UNLIKELY","joy":"VERY_LIKELY","sorrow":"VERY_UNLIKELY","surprise":"VERY_UNLIKELY"}}'
-var emotions_AUs;
